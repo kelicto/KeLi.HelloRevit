079cd75632d03e52149ab9c7434c818a3f8f33c3
KeLi <kelicto@protonmail.com>
2021-11-25 00:08:57 +0800

    Updates code.

---
+++ KeLi.HelloRevit/HelloRevitCmd.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace KeLi.HelloRevit
+{
+    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.Manual)]
+    public class HelloRevitCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Title", "Hello, Revit!");
+
+            return Result.Succeeded;
+        }
+    }
+}
